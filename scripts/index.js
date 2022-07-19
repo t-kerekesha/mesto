c4c4600cd49d0633486aeaa354ea256831c4f6da
Tanya <t.kerekesha@yandex.ru>
2022-07-19 16:28:49 +0300

    fix: fix the review remark

--- scripts/index.js
+++ scripts/index.js
@@ -3,30 +3,21 @@ const popup = document.querySelector('.popup');
 const profile = document.querySelector('.profile');
 const closeButton = popup.querySelector('.popup__close-button');
 const saveButton = popup.querySelector('.popup__save-button');
+const popupContainer = popup.querySelector('.popup__container');
 const editButton = profile.querySelector('.profile__edit-button');
-const addButton = profile.querySelector('.profile__add-button');
 let profileName = profile.querySelector('.profile__name');
 let profileAbout = profile.querySelector('.profile__about');
 let inputName = popup.querySelector('.popup__input_type_name');
 let inputAbout = popup.querySelector('.popup__input_type_about');
-const galeryList = document.querySelector('.galery__list');
 
 function popupOpen() {
   popup.classList.add('popup_opened');
   writeDataPopup();
   inputName.focus();
-  page.style.overflowY = 'hidden'; //убрать прокрутку страницы
 }
 
 function popupClose () {
   popup.classList.remove('popup_opened');
-  page.style.overflowY = '';
-}
-
-function popupCloseLostFocus(event) {
-  if (event.target === event.currentTarget) {
-    popupClose();
-  }
 }
 
 function writeDataPopup () {
@@ -41,32 +32,9 @@ function formSubmitHandler (evt) {
   popupClose();
 }
 
-function addImageInGalery () {
-  galeryList.insertAdjacentHTML('beforeend', `
-    <li>
-      <figure class="galery__container">
-        <div class="galery__aspect-ratio">
-          <img src="./images/dombay.jpg" alt="Домбай" class="galery__image">
-        </div>
-        <figcaption class="galery__container-caption">
-          <h2 class="galery__caption">Домбай</h2>
-          <button class="galery__like-button"></button>
-        </figcaption>
-      </figure>
-    </li>
-  `);
-} // заготовка для вставки
-
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
-popup.addEventListener('click', popupCloseLostFocus);
-popup.addEventListener('submit', formSubmitHandler);
-popup.addEventListener('keydown', function(event) {
-  if (event.key === 'Escape') {
-    popupClose();
-  }
-});
+popupContainer.addEventListener('submit', formSubmitHandler);
 
-addButton.addEventListener('click', addImageInGalery);
 
 
