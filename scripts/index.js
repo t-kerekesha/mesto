b8f078b1841c19709bda450ad21409d926ec405c
Tanya <t.kerekesha@yandex.ru>
2022-07-23 14:42:15 +0300

    feat: add initial image

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,67 @@
-const page = document.querySelector('.page');
+// const page = document.querySelector('.page');
 const popup = document.querySelector('.popup');
 const profile = document.querySelector('.profile');
+const galeryList = document.querySelector('.galery__list');
 const closeButton = popup.querySelector('.popup__close-button');
-const saveButton = popup.querySelector('.popup__save-button');
 const popupContainer = popup.querySelector('.popup__container');
 const editButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileAbout = profile.querySelector('.profile__about');
 let inputName = popup.querySelector('.popup__input_type_name');
 let inputAbout = popup.querySelector('.popup__input_type_about');
+const galeryItemTemplate = document.querySelector('#galery-item-template').content;
+let galeryItem;
+
+// начальные картинки
+const initialItems = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// добавление начальных картинок
+
+initialItems.forEach(function(item) {
+  galeryItem = galeryItemTemplate.cloneNode(true);
+  galeryItem.querySelector('.galery__image').src = item.link;
+  galeryItem.querySelector('.galery__caption').textContent = item.name;
+  galeryList.prepend(galeryItem);
+});
+
+// функции работы с попап
 
 function popupOpen() {
   popup.classList.add('popup_opened');
