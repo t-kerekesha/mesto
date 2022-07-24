91499aaabcc39191da14f7c2fb0a30eb69969175
Tanya <t.kerekesha@yandex.ru>
2022-07-24 21:09:43 +0300

    feat: add images

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,71 @@
-// const page = document.querySelector('.page');
-const popup = document.querySelector('.popup');
 const profile = document.querySelector('.profile');
-const galeryList = document.querySelector('.gallery__list');
-const closeButton = popup.querySelector('.popup__close-button');
-const popupContainer = popup.querySelector('.popup__container');
 const editButton = profile.querySelector('.profile__edit-button');
-let profileName = profile.querySelector('.profile__name');
-let profileAbout = profile.querySelector('.profile__about');
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputAbout = popup.querySelector('.popup__input_type_about');
+const addButton = profile.querySelector('.profile__add-button');
+const profileName = profile.querySelector('.profile__name');
+const profileAbout = profile.querySelector('.profile__about');
+
+const galeryList = document.querySelector('.gallery__list');
 const galleryItemTemplate = document.querySelector('#gallery-item-template').content;
 let galeryItem;
+
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const inputName = popupEditProfile.querySelector('.popup__input_type_name');
+const inputAbout = popupEditProfile.querySelector('.popup__input_type_about');
+
+const popupAddImage = document.querySelector('.popup_type_add-image');
+const inputTitle = popupAddImage.querySelector('.popup__input_type_name');
+const inputLink = popupAddImage.querySelector('.popup__input_type_link');
+
+const popupContainers = document.querySelectorAll('.popup__container');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 
 // начальные картинки
 const initialItems = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Южный Урал',
+    link: './images/south-ural-kurumnik.jpg'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Териберка, Баренцово море',
+    link: './images/teriberka.jpg'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Алтай',
+    link: './images/altai-mars.jpg'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Карелия',
+    link: './images/kareliya.jpg'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Южный Урал',
+    link: './images/south-ural.jpg'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Алтай',
+    link: './images/altai.jpg'
   }
 ];
 
 // добавление начальных картинок
+initialItems.forEach(function (item) {
+  addImageInGalery(item.name, item.link)
+});
 
-initialItems.forEach(function(item) {
+// добавление картинок
+function addImageInGalery(name, link) {
   galeryItem = galleryItemTemplate.cloneNode(true);
-  galeryItem.querySelector('.gallery__image').src = item.link;
-  galeryItem.querySelector('.gallery__caption').textContent = item.name;
+  galeryItem.querySelector('.gallery__image').src = link;
+  galeryItem.querySelector('.gallery__image').alt = name;
+  galeryItem.querySelector('.gallery__caption').textContent = name;
   galeryItem.querySelector('.gallery__delete-button').addEventListener('click', deleteImageFromGallery);
   galeryItem.querySelector('.gallery__like-button').addEventListener('click', likeImage);
   galeryList.prepend(galeryItem);
-});
+}
 
 // удаление картинок
 function deleteImageFromGallery(event) {
-  let item = event.target.closest('li');
+  let item = event.target.closest('.gallery__list-item');
   item.remove();
 }
 
@@ -75,29 +75,54 @@ function likeImage(event) {
 }
 
 // функции работы с попап
-
-function popupOpen() {
+function openPopup(popup) {
   popup.classList.add('popup_opened');
-  writeDataPopup();
-  inputName.focus();
 }
 
-function popupClose () {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function writeDataPopup () {
+function writeDataProfile() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
 
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // отмена стандартной отправки формы
+function saveDataProfile() {
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  popupClose();
 }
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-popupContainer.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', function () {
+  openPopup(popupEditProfile);
+  writeDataProfile();
+  inputName.focus();
+});
+
+addButton.addEventListener('click', function () {
+  openPopup(popupAddImage);
+  inputTitle.focus();
+});
+
+popupContainers.forEach(function (form) {
+  form.addEventListener('submit', formSubmitHandler);
+});
+
+function formSubmitHandler(event) {
+  event.preventDefault(); // отмена стандартной отправки формы
+    if (event.target.closest('.popup') === popupEditProfile) {
+      saveDataProfile();
+    }
+    else if (event.target.closest('.popup') === popupAddImage) {
+      addImageInGalery(inputTitle.value, inputLink.value);
+      inputTitle.value = '';
+      inputLink.value = '';
+    }
+    closePopup(event.target.closest('.popup'));
+}
+
+closeButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    closePopup(event.target.closest('.popup'));
+  });
+});
